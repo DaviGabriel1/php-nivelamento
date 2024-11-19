ebcacabe35918fcdcbdd4fb1186623851b47116d
DaviGabriel1 <davig41441503@gmail.com>
2024-11-19 12:52:28 -0300

    materialize css

--- cursolaravel/resources/views/site/home.blade.php
+++ cursolaravel/resources/views/site/home.blade.php
@@ -2,74 +2,76 @@
 @section('title',"essa é a página home")
 
 @section('conteudo')
-    <h1>essa é a home</h1>
 
-    {{-- isso é um comentário --}}
+<div class="row container">
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+                <span class="card-title">Card Title</span>
+                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col s12 m3">
+        <div class="card">
+            <div class="card-image">
+              <img src="images/sample-1.jpg">
+              <span class="card-title">Card Title</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+            </div>
+        </div>
+    </div>
 
 
-    {{-- ESTRUTURA CONDICIONAL--}}
-
-    {{ isset($nome) ? "existe" : "não existe"}}
-    {{$teste ?? "padrão"}}
-
-    @if("1" == 1)
-        <h1>if no blade</h1>
-    @endif
-
-    @unless ("1" === 1)
-        <h1>imprime se a preposição não for verdadeira</h1>
-    @endunless
-
-    @switch($idade)
-        @case(1)
-                <h1>idade errada</h1>
-            @break
-        @case(19)
-                <h1>idade certa</h1>
-            @break
-        @default
-            <h1>nenhuma idade certa</h1>
-    @endswitch
-
-    @guest
-        <h1>imprime se não tiver um usuario autenticado</h1>
-    @endguest
-
-    @auth
-        <h1>imprime se tiver um usuario autenticado</h1>
-    @endauth
-
-    {{-- ESTRUTURA DE REPETIÇÃO --}}
-
-    @for($i = 1;$i<=10;$i++)
-        <h3> {{$i}} <br></h3>
-    @endfor
-
-    @php
-        $j = 1;
-    @endphp
-
-    @while ($j <=10)
-        <h3> {{$j}} <br></h3>
-        @php $j++ @endphp
-    @endwhile
-
-    @foreach ($frutas as $fruta)
-        <span>{{$fruta}} , </span>
-    @endforeach
-
-    @forelse ($frutas as $fruta)
-        <span>{{$fruta}} , </span>
-        @empty
-        <h1>não tem frutas</h1>
-    @endforelse
-
-    @include("includes.mensagem",['titulo' => 'Mensagem de sucesso'])
-
-    @component('components.sidebar')
-        @slot('paragrafo')
-            texto qualquer vindo do slot
-        @endslot
-    @endcomponent
-
+</div>
 @endsection
--- cursolaravel/resources/views/site/layout.blade.php
+++ cursolaravel/resources/views/site/layout.blade.php
@@ -3,8 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>@yield("title")</title>
 </head>
 <body>
 
+  <nav class="red">
+    <div class="nav-wrapper container">
+      <a href="#" class="brand-logo center">Curso Laravel</a>
+      <ul id="nav-mobile" class="left">
+        <li><a href="">home</a></li>
+        <li><a href="">carrinho</a></li>
+      </ul>
+    </div>
+  </nav>
+
 @yield('conteudo')
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
