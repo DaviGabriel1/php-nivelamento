5e6cde740fc39d9da2c44dfff891b7d80395a7dd
DaviGabriel1 <davig41441503@gmail.com>
2024-11-21 08:40:07 -0300

    belongs to e oneToMany para busca por chave estrangeira

--- cursolaravel/resources/views/site/details.blade.php
+++ cursolaravel/resources/views/site/details.blade.php
@@ -1,5 +1,5 @@
-@extends("site.layout");
-@section("title","Detalhes");
+@extends("site.layout")
+@section("title","Detalhes")
 @section("conteudo")
 
 <div class="row container">
@@ -9,6 +9,8 @@
     <div class="col s12 m6">
         <h1>{{$produto->nome}}</h1>
         <p>{{$produto->descricao}}</p>
+        <p>Postado por: {{ $produto->user->firstName}}<br>
+            Categoria: {{$produto->categoria->nome}}</p>
         <button class="btn orange btn-large"> Comprar </button>
     </div>
 </div>
