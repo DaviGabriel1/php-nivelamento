505678e7d3894fcf60280c4d3294e439da4bf902
DaviGabriel1 <davig41441503@gmail.com>
2024-11-21 08:26:45 -0300

    busca de um unico produto

--- cursolaravel/resources/views/site/home.blade.php
+++ cursolaravel/resources/views/site/home.blade.php
@@ -10,18 +10,17 @@
                 <div class="card-image">
                 <img src="{{$produto->imagem}}">
                     <span class="card-title">{{$produto->name}}</span>
-                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+                    <a href="{{route('site.details',$produto->slug)}}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
                 </div>
                 <div class="card-content">
                 <p>{{Str::limit($produto->descricao,50)}}</p>
                 </div>
-                <div class="card-action">
-                <a href="#">This is a link</a>
-                </div>
             </div>
         </div>
     @endforeach
+</div>
 
-
+<div class="row">
+    {{$produtos->links("custom.pagination")}}
 </div>
 @endsection
--- cursolaravel/routes/web.php
+++ cursolaravel/routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource("produtos",ProdutoController::class); #identifica o index
+Route::get("/",[SiteController::class,"index"])->name("site.index");
+Route::get("/produto/{slug}",[SiteController::class,"details"])->name("site.details");
 
 /*
 Route::get('/', function () {
